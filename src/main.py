04565e7929687da56fa26a27daf885db8647edc7
Daniel Bailo <danbailoufms@gmail.com>
2019-12-09 15:39:27 -0300

    integracao dos arquivos csv

--- src/main.py
+++ src/main.py
@@ -1,13 +1,16 @@
-from core import BMF
+from core import BMF, CSV
 
 if __name__ == "__main__":
-	data = {
+	date = {
 		'dData1': '12/05/2019'
 	}
 
-	bmf = BMF('http://www2.bmf.com.br/pages/portal/bmfbovespa/lumis/lum-tipo-de-participante-enUS.asp', data)
+	bmf = BMF('http://www2.bmf.com.br/pages/portal/bmfbovespa/lumis/lum-tipo-de-participante-enUS.asp', date)
 
 	path = "../filters/contract.txt"
 
 	bmf.get_filters(path)
 	bmf.get_data()
+
+	csv = CSV(bmf.data, date, bmf.filters)
+	csv.write()
