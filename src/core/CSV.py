6fd39de60fd7a010df39ffdf30e1a2b177baca13
Daniel Bailo <danbailoufms@gmail.com>
2019-12-10 19:48:29 -0300

    programando o csv de acumulado

--- src/core/CSV.py
+++ src/core/CSV.py
@@ -63,28 +63,23 @@ class CSV:
 
 
 		print(json.dumps(accumulated, indent=4))
-		exit()		
-		
-		for contract, rows in all_data.items():
-			fields = list(rows.keys())
-			temp = []
-			with open(path + contract + ".csv", mode="w") as csv_file:
-				writer = csv.DictWriter(csv_file, fieldnames=fields, delimiter=";")
-				writer.writeheader()
-				for row in rows.values():
-					temp.append(row)
-				data = list(zip(*temp))
-				for value in data:
-					writer.writerow({
-						fields[0]: value[0],
-						fields[1]: value[1],
-						fields[2]: value[2],
-						fields[3]: value[3],
-						fields[4]: value[4],
-						fields[5]: value[5],
-						fields[6]: value[6],
-						fields[7]: value[7],
-						fields[8]: value[8]
+		with open(path + contract + "_ACUMULADO.csv", mode="w") as csv_file:
+			writer = csv.DictWriter(csv_file, fieldnames=fields, delimiter=";")
+			writer.writeheader()
+			for row in rows.values():
+				temp.append(row)
+			data = list(zip(*temp))
+			for value in data:
+				writer.writerow({
+					fields[0]: value[0],
+					fields[1]: value[1],
+					fields[2]: value[2],
+					fields[3]: value[3],
+					fields[4]: value[4],
+					fields[5]: value[5],
+					fields[6]: value[6],
+					fields[7]: value[7],
+					fields[8]: value[8]
 					})		
 
 	def get_accumulated(self, path=os.path.join("..","csv","")):
