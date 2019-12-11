9ac4350e140b7a10591d91bd7196b1efea6b79a2
Daniel Bailo <danbailoufms@gmail.com>
2019-12-10 21:47:51 -0300

    gerando corretamente os csv, acho q vou tirar a coluna de identificador e colocar somente no banco

--- src/core/CSV.py
+++ src/core/CSV.py
@@ -31,56 +31,58 @@ class CSV:
 						fields[8]: value[8]
 					})
 
+	def write2(self, all_data, path=os.path.join("..","csv","accumulated","")):
+		for contract, rows in all_data.items():
+			fields = ["DATA", "PARTICIPANTE", "SALDO", "ACUMULADO"]
+			accumulated = defaultdict(int)
+			temp = []
+			with open(path + contract + "_ACUMULADO.csv", mode="w") as csv_file:
+				writer = csv.DictWriter(csv_file, fieldnames=fields, delimiter=";")
+				writer.writeheader()
+				for row in rows.values():
+					temp.append(row)
+				data = list(zip(*temp))
+				for value in data:
+					accumulated[value[3]] += value[8]
+					writer.writerow({
+						fields[0]: value[1],
+						fields[1]: value[3],
+						fields[2]: value[8],
+						fields[3]: accumulated[value[3]]
+					})
 	def write_accumulated(self, all_data, path=os.path.join("..","csv","accumulated","")):
-		#accumulated = {}
-		#keys = ["DATA", "SALDO"]
-		#key_participant = "PARTICIPANTE"
-		#for contract in all_data:
-		#	temp = []			
-		#	#accumulated[all_data[contract][participant]] = 0
-		#	for participant in all_data[contract][key_participant]:
-		#		
-		#	for key in keys:
-#
-		#		#accumulated[all_data[contract][participant]][] += 0
-		#		temp.append(all_data[contract][key])
-		#	print(list(zip(*temp)))
-		#	print(accumulated)
-		#	exit()
-		
-		#accumulated = defaultdict(lambda: defaultdict(list))
 		accumulated = defaultdict(lambda: defaultdict(lambda: defaultdict(int)))
-
 		keys = ["DATA", "PARTICIPANTE", "SALDO"]
 		for contract in all_data:
-			temp = []			
+			temp = []
 			for key in keys:
 				temp.append(all_data[contract][key])
-			
 			data = list(zip(*temp))
+
 			for row in data:
 				accumulated[contract][row[0]][row[1]] += row[2]
 
 
 		print(json.dumps(accumulated, indent=4))
+
+		exit()
 		with open(path + contract + "_ACUMULADO.csv", mode="w") as csv_file:
-			writer = csv.DictWriter(csv_file, fieldnames=fields, delimiter=";")
+			writer = csv.DictWriter(csv_file, fieldnames=keys, delimiter=";")
 			writer.writeheader()
-			for row in rows.values():
-				temp.append(row)
-			data = list(zip(*temp))
-			for value in data:
-				writer.writerow({
-					fields[0]: value[0],
-					fields[1]: value[1],
-					fields[2]: value[2],
-					fields[3]: value[3],
-					fields[4]: value[4],
-					fields[5]: value[5],
-					fields[6]: value[6],
-					fields[7]: value[7],
-					fields[8]: value[8]
-					})		
+			for k,v in accumulated.items():
+				print(k,v)
+#			for value in data:
+#				writer.writerow({
+#					fields[0]: value[0],
+#					fields[1]: value[1],
+#					fields[2]: value[2],
+#					fields[3]: value[3],
+#					fields[4]: value[4],
+#					fields[5]: value[5],
+#					fields[6]: value[6],
+#					fields[7]: value[7],
+#					fields[8]: value[8]
+#					})		
 
 	def get_accumulated(self, path=os.path.join("..","csv","")):
 		all_files = [temp_file for temp_file in os.listdir(path) if temp_file[-4:] == ".csv"]		
